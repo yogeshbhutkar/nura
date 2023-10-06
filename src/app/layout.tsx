75c8970c8393aa0d7d96f9f3d0b386a21402d89f
yogeshbhutkar <pseudonetwork1001@gmail.com>
2023-10-06 11:30:24 +0530

    Removed the default favicon and added a custom made icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Providers from "@/components/Providers";
 import "react-loading-skeleton/dist/skeleton.css";
 import "simplebar-react/dist/simplebar.min.css";
 import { Toaster } from "@/components/ui/toaster";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
