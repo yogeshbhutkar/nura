aff6e16f3b79400a05d991d312455ae3cd0e7c63
suprabathk <suprabathkondapally@gmail.com>
2023-10-06 12:24:43 +0530

    Landing page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,9 @@ import { ArrowRight } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+        <div className="flex h-14 items-center justify-between">
           <Link href={"/"} className="flex z-40 font-semibold ">
             <span>Nura.</span>
           </Link>
@@ -17,15 +17,6 @@ export default function Navbar() {
 
           <div className="hidden items-center space-x-4 sm:flex">
             <>
-              <Link
-                href={"/pricing"}
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}
-              >
-                Pricing
-              </Link>
               <LoginLink
                 className={buttonVariants({
                   variant: "ghost",
@@ -34,14 +25,13 @@ export default function Navbar() {
               >
                 Sign in
               </LoginLink>
-              <RegisterLink
-                className={buttonVariants({
-                  size: "sm",
-                })}
-              >
-                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
-              </RegisterLink>
             </>
+            <Link
+              href="/dashboard"
+              className="rounded-full transition-all flex items-center hover:bg-green-500 border border-green-500 text-green-500  hover:text-white py-1 px-4 "
+            >
+              Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+            </Link>
           </div>
         </div>
       </MaxWidthWrapper>
