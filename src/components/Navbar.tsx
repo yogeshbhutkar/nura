a74e42c6d2e74c5666b55e0ba7b2a14875803bf0
yogeshbhutkar <pseudonetwork1001@gmail.com>
2023-10-06 11:06:41 +0530

    Updated  metadata

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href={"/"} className="flex z-40 font-semibold ">
-            <span>quill.</span>
+            <span>Nura.</span>
           </Link>
           {/* TODO: Add mobile navbar */}
 
